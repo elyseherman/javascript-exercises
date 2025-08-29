607fa363108b187ee913e08ee73562e95e6c16c7
Elyse Herman <elyse_herman@hotmail.com>
2025-08-29 13:47:48 -0400

    Completed questions 14 and 15

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+
+    let count;
+    if (typeof n == 'string') {
+        count = parseInt(n)
+    } else {
+        count = n;
+    }
+
+    if (n < 0) {
+        return 'OOPS'
+    }
+
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return 1;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+    let current;
+    for (let i = 1; i < n; i++) {
+        current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return current;
 
 };
 
