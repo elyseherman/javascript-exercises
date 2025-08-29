30573f8cc70c8993d1328945a4f3306ab89c473c
Elyse Herman <elyse_herman@hotmail.com>
2025-08-29 13:30:59 -0400

    Completed questions 12 and 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, cur) => {
+    return prev * cur;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+
+  function computeFactorial(x) {
+    if (x == 0 || x == 1) {
+      return 1
+    }
+
+    return x * computeFactorial(x - 1)
+  }
+
+  return computeFactorial(x)
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let i = 0;
+    let j = str.length - 1;
+
+    while (i <= j) {
+        if (/^[A-Za-z0-9]$/.test(str[i]) && /^[A-Za-z0-9]$/.test(str[j])) {
+            if (str[i].toLowerCase() != str[j].toLowerCase()) {
+                return false
+            }
+            else {
+                i += 1;
+                j -= 1;
+            }
+        } else if (! /^[A-Za-z0-9]$/.test(str[i])) {
+            i++;
+        }
+        else if (! /^[A-Za-z0-9]$/.test(str[j])) {
+            j--;
+        }
+    }
+
+    return true;
 
 };
 
